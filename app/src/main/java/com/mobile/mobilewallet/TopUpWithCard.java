5f0460a4cde80ade2c97a4d24d9eba5cc8504606
dark-royal <praiseoyewole560@gmail.com>
2024-07-28 12:09:19 +0100

    agent registration for now

---
+++ app/src/main/java/com/mobile/mobilewallet/TopUpWithCard.java
@@ -0,0 +1,13 @@
+package com.mobile.mobilewallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TopUpWithCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_up_with_card);
+    }
+}
