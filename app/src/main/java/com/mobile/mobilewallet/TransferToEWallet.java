6260f586cb75f89e79405146e5f8fefd4dc60dc1
dark-royal <praiseoyewole560@gmail.com>
2024-07-28 17:25:03 +0100

    agent registration for now

---
+++ app/src/main/java/com/mobile/mobilewallet/TransferToEWallet.java
@@ -0,0 +1,13 @@
+package com.mobile.mobilewallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TransferToEWallet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transfer_to_ewallet);
+    }
+}
