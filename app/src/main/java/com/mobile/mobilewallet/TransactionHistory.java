192e30cff7de86d27357287987d8d32c7cdce726
dark-royal <praiseoyewole560@gmail.com>
2024-07-29 05:31:16 +0100

    i israel am commiting

--- app/src/main/java/com/mobile/mobilewallet/TransactionHistory.java
+++ app/src/main/java/com/mobile/mobilewallet/TransactionHistory.java
@@ -2,6 +2,8 @@ package com.mobile.mobilewallet;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.mobile.mobilewallet.helperclass.ContextDeterminer;
@@ -13,6 +15,11 @@ public class TransactionHistory extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transactions_history);
 
+        Spinner spinner = findViewById(R.id.categoryDrop);
+        String[] items = {"Item 1", "Item 2", "Item 3"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.activity_transactions_history, R.id.spinner_button, items);
+        spinner.setAdapter(adapter);
     }
 
     public void nextScreen(View view){
